2e1bb7f376525c6a29576a2d40d9916669ca3ed4
Mateus Nunes <mateusnss@protonmail.ch>
2021-12-17 15:08:06 -0300

    adicionado meuPrimeiroProgramaJava

---
+++ java/meuPrimeiroProgramaJava/meuPrimeiroProgramaJava.java
@@ -0,0 +1,30 @@
+
+/// salvar como meuPrimeiroPrograma.java
+/// Author: Mateus Nunes
+
+// nome da classe
+class MeuPrimeiroProgramaJava
+{
+
+	// módulo principal com a entrada pela linha de comando
+	public static void main(String entrada[])
+	{
+		//declaração de variáveis
+		int inteiro = 17;
+		char caracter = 'N';
+		String frase = "Mateus Nunes";
+		boolean VF = true;
+
+		
+		if (VF == true)
+		{
+			System.out.println("Eu sou " + frase + ", tenho " + inteiro + " anos de idade.");
+
+		}
+		
+		System.exit(0);
+
+	}
+
+}
+
